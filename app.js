d578a6ebb58006913c6e43ac1496aa1adb401ba2
maheshwss <maheshy@wharfstreetstrategies.com>
2022-10-07 17:12:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    x : 10,
+    y : 20,
+    z : function() {
+        return 30;
+    }
+};  // exporting the data to index.js
---
+++ index.js
@@ -0,0 +1,34 @@
+const app = require('./app')  // improting the data from app.js file
+
+// var x = '20';
+// console.log(x);
+// console.warn(x);
+
+// console.log(app);
+// console.log(app.x);
+// console.log(app.y);
+// console.log(app.z());
+
+// const arr = [24,6,8,9,1,5,3];
+
+// const result = arr.filter(item => item>5);
+// console.log(result);
+
+// core modules
+// console, fs, Buffer, HTTP, etc
+
+// global modules - do not need to import, directly available
+// console
+
+// non global modules - need to import
+// fs
+
+// const fs = require('fs');
+// fs.writeFileSync("hello.txt","node js");
+
+// console.log("-->>",__dirname);
+// console.log("-->>",__filename); 
+
+// const fs = require('fs').writeFileSync;
+// fs("abc.txt", "abc");
+
